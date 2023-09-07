9300aec8649ad22733ca015bc15f24cbd09a9289
Robinson Davila <rdavila@seg-pichincha.com>
2023-09-07 09:42:34 -0500

    Se agrega el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mi_Primera_Aplicacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
